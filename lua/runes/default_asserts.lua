e073c7e3e52f652bb685aa9d26085d50e642d76d
Henry Kupty <hkupty@gmail.com>
2022-05-17 23:50:37 +0200

    Add test asserts

--- lua/runes/default_asserts.lua
+++ lua/runes/default_asserts.lua
@@ -1,5 +1,6 @@
 return vim.tbl_extend("keep",
   require("runes.asserts.arithmetic"),
+  require("runes.asserts.tables"),
   require("runes.asserts.logical"),
   require("runes.asserts.types"),
   require("runes.asserts.neovim")
