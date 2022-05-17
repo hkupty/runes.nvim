470e316631773904314da708d3394400804d67ad
Henry Kupty <hkupty@gmail.com>
2022-05-17 23:51:20 +0200

    Improve state working
    Promote run logs to a structured data object

--- lua/runes/state.lua
+++ lua/runes/state.lua
@@ -3,6 +3,13 @@ local assert = require("runes.asserts")
 
 local state = {}
 
+state.status_codes = {
+  -- skip = -1 -- TODO mark skip as a status code instead of removing from the list
+  success = 0,
+  fail = 1,
+  error = 2,
+}
+
 state.with_new_context = function(tbl, k)
   local new = vim.deepcopy(tbl)
   new.context = k
@@ -10,11 +17,60 @@ state.with_new_context = function(tbl, k)
   return new
 end
 
+-- Standardize message format
+state.run_log = function(opt)
+  return {
+      status = opt.status,
+      error_message = opt.error_message,
+      context = opt.context,
+      args = opt.args,
+      assert = opt.assert,
+      phase = opt.phase
+    }
+end
+
+state.success_run = function(opt)
+  return {
+    status = state.status_codes.success,
+    context = opt.context,
+    args = opt.args,
+    assert = opt.assert,
+    phase = opt.phase
+  }
+end
+
+state.failed_run = function(opt)
+  return {
+    status = state.status_codes.fail,
+    error_message = opt.error_message,
+    context = opt.context,
+    args = opt.args,
+    assert = opt.assert,
+    phase = opt.phase
+  }
+end
+
+state.error_run = function(opt)
+  return {
+    status = state.status_codes.error,
+    error_message = opt.error_message,
+    context = opt.context,
+    args = opt.args,
+    assert = opt.assert,
+    phase = opt.phase
+  }
+end
+
+state.run_failed = function(run)
+  return run.status ~= nil and run.status > state.status_codes.success
+end
+
 state.statemaker = function(case, initial_state)
+
   local test_state = vim.tbl_extend("keep", case,
     {
     run = {},
-    result = {success = true},
+    result = {},
     phase = "init",
 
 
@@ -35,8 +91,8 @@ state.statemaker = function(case, initial_state)
     if context == nil then
       context = key
     end
-    local run = {
-      success = result,
+    local run = state.run_log{
+      status = result and state.status_codes.success or state.status_codes.fail,
       error_message = errmsg,
       context = context,
       args = {...},
